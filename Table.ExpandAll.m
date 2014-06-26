f80b7c2bd427e4ea29113a089722efc5f27b693f
tycho01 <tychogrouwstra@gmail.com>
2014-06-26 17:41:09 +0800

    fixed ExpandAll

--- Table.ExpandAll.m
+++ Table.ExpandAll.m
@@ -4,14 +4,18 @@
 //Usage:
     Table.ExpandAll = Load("Table.ExpandAll"),
     xml = Xml.Tables("<books><book><name>Book1</name><pages><page>1</page><page>2</page><page>3</page></pages></book><book><name>Book2</name><pages><page>1</page><page>2</page><page>3</page></pages></book></books>"),
-    Table.ExpandAll(xml)	//, null, false
+    Table.ExpandAll(xml)	//, null, true
 //Result: [an expanded version of the given table with nested records/tables]
 */
 
-let Table.ExpandAll = (TableToExpand as table, optional ColumnNumber as number, optional AppendParentNames as logical) =>
+let Table.ExpandAll = (
+	TableToExpand as table,					//the table you wish to expand
+	optional ColumnNumber as number,		//the column number to expand
+	optional AppendParentNames as logical	//whether to use append parent column names e.g. "ul.li", or just keep "li" where possible (reverting to the qualified name in case of a colum name clash)
+) =>
 let
 	ColumnNumber = if (ColumnNumber=null) then 0 else ColumnNumber,
-	AppendParentNames = if (AppendParentNames=null) then true else AppendParentNames
+	AppendParentNames = if (AppendParentNames=null) then false else AppendParentNames
 in
 
 List.Last(
@@ -22,10 +26,11 @@ List.Last(
 		],
 		each [col]<=(Table.ColumnCount([Tbl])-1),
 		each let
-			ColumnName = Table.ColumnNames([Tbl]){[col]},
+			ColumnNames = Table.ColumnNames([Tbl]),
+			ColumnName = ColumnNames{[col]},
 			ColumnsToExpand = List.Distinct(List.Combine(List.Transform(Table.Column([Tbl], ColumnName),
 				each if _ is table then Table.ColumnNames(_) else {}))),
-			NewColumnNames = List.Transform(ColumnsToExpand, each if AppendParentNames then ColumnName & "." & _ else _),
+			NewColumnNames = List.Transform(ColumnsToExpand, each if (AppendParentNames or List.Contains(ColumnNames,_)) then ColumnName & "." & _ else _),
 			CanExpandCol = List.Count(ColumnsToExpand)>0
 		in [
 			Tbl = if CanExpandCol
