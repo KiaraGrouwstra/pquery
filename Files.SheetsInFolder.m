6e8d35eaa8db5e9a21b9dd4cd69d996292ec59a8
tycho01 <tychogrouwstra@gmail.com>
2014-09-11 14:32:45 +0800

    added new one

---
+++ Files.SheetsInFolder.m
@@ -0,0 +1,19 @@
+/*
+//Create a table with info on all sheets in any Excel files in a particular folder.
+//Usage:
+    Files.SheetsInFolder = Load("Files.SheetsInFolder"),
+    Source = SheetsInFolder("C:\path\to\my\folder\")
+//Result: [a table containing the binary file content, file names, sheet tables, and sheet names for each sheet in each spreadsheet in the given folder]
+*/
+let SheetsInFolder = (folderPath as text) =>
+let
+    Source = Folder.Files(folderPath),
+    FilteredRows = Table.SelectRows(Source, each Text.Start("[Extension]",3) = ".xl"),
+    RemovedOtherColumns = Table.SelectColumns(FilteredRows,{"Content", "Name"}),
+    NoTemps = Table.SelectRows(RemovedOtherColumns, each not Text.StartsWith([Name], "~$")),
+    InsertedCustom = Table.AddColumn(NoTemps, "Sheets", each Excel.Workbook([Content])),
+    Expanded = Table.ExpandTableColumn(InsertedCustom, "Sheets", {"Data", "Name"}, {"D","N"}),
+    NoPrintAreas = Table.SelectRows(Expanded, each not Text.Contains([N], "$"))
+in
+    NoPrintAreas
+in SheetsInFolder
