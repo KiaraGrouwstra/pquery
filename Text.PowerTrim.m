b7123c1e9bd28da1cefe795714ef9ef640fe34b3
Ivan Bondarenko <bondarenko.ivan@me.com>
2015-10-10 22:23:28 +0300

    Update Text.PowerTrim.m

--- Text.PowerTrim.m
+++ Text.PowerTrim.m
@@ -2,7 +2,9 @@
   Function removes double presents of specified characters.
   By default remove double spaces and leading + ending spaces.
   Like TRIM function in Excel
-
+  
+  Original is taked from Ken Puls's blog
+  http://www.excelguru.ca/blog/2015/10/08/clean-whitespace-in-powerquery/
 */
 
 (text as text, optional char_to_trim as text) =>
