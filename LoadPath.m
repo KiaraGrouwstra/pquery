1113ceb0b2b8bbe9d0344713f272781e4f0a3120
Ivan Bondarenko <bondarenko.ivan@me.com>
2015-10-13 12:45:09 +0300

    Delete LoadPath.m

--- LoadPath.m
+++
@@ -1,6 +0,0 @@
-﻿let
-    //Feel free to just put your own fixed path in here instead of using the table from this workbook!
-    //Source = "D:\pquery\"
-    Source = Excel.CurrentWorkbook(){[Name="Table1"]}[Content]{0}[Path]
-in
-    Source
