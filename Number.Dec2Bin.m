cd7312f1be5aaa52aeac2b549677897adc995a3d
Ivan Bondarenko <bondarenko.ivan@me.com>
2016-12-10 17:34:39 +0300

    Update and rename Number.ToBinaryString.m to Number.Dec2Bin.m
    Rename and add link to Excel function

--- Number.Dec2Bin.m
+++ Number.Dec2Bin.m
@@ -3,6 +3,9 @@
 // Usage:
 // Number.ToBinaryString( 1026 )
 // result: 10000000010
+// Same as Excel funciton DEC2BIN
+// https://support.office.com/en-us/article/DEC2BIN-function-0f63dd0e-5d1a-42d8-b511-5bf5c6d43838
+//
 
 (num as number, optional string as nullable text) =>
     let
