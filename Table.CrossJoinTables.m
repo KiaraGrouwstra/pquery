ff76fcbb7b3342c3c6a91853905f5aede2e5d225
Maxim Zelensky <zelensky@yandex.ru>
2017-08-09 13:08:43 +0300

    New version of Table.CrossJoinTables (with Join)

--- Table.CrossJoinTables.m
+++ Table.CrossJoinTables.m
@@ -7,12 +7,26 @@
       #table({"C","D"},{{"E",3},{"F",4}})
     })
 // Result: a cross-joined version of all the tables. beware of performance, the result could get big!
+// Revision: Maxim Zelensky Aug 09, 2017
+// Revision purpose: use Table.Join instead of Table.ExpandTableColumn (used in referenced function) to get performance gain
 */
 
-(Tables as list) as table =>
-let
-  Table.ExpandTables = Load("Table.ExpandTables"),
-  Cols = List.Transform(List.Numbers(1, List.Count(Tables)), each Number.ToText(_)),
-  Source = Table.ExpandTables(#table(Cols, {Tables}))
-in
-Source
+(tables as list) as table =>
+    let 
+        CrossJoin = List.Accumulate(
+            List.Positions(tables), 
+            #table({},{}), 
+            (state, current) => 
+                let
+                    CurrentTable = tables{current}, // need check for table type?
+                    CurrentNames = Table.ColumnNames(state),
+                    OldNames = Table.ColumnNames(CurrentTable),
+                    NewNames = List.Transform(OldNames, each if List.Contains(CurrentNames, _) then Text.From(current+1) & "_" & _ else _),
+                    Renamed = Table.RenameColumns(CurrentTable, List.Zip({OldNames, NewNames}))
+                in
+                    Table.Join(state, {}, Renamed, {}, JoinKind.FullOuter)
+            )
+    in
+        if List.IsEmpty(tables) 
+            then #table({},{}) 
+            else CrossJoin
